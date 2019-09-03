6a1280c0a1106ecc566e46aa8d31f9972fe2ac8a
waqar younis <waqaryounis7564@gmail.com>
2019-09-03 12:02:11 +0500

    included all http actions to genre api

--- app.js
+++ app.js
@@ -6,12 +6,12 @@ const uniqid = require("uniqid");
 
 app.use(bodyParser.json());
 
-const genres = [];
-
+const genres = [{ name: "animted", id: "5a" }];
+//getting genre
 app.get("/genres/all", (req, res) => {
   res.send(genres);
 });
-
+//posting genre
 app.post("/genres", (req, res) => {
   const schema = {
     name: Joi.string()
@@ -25,9 +25,29 @@ app.post("/genres", (req, res) => {
   genres.push(genre);
   res.send(genre);
 });
+//editing genre
+app.put("/genres/:id", (req, res) => {
+  const schema = {
+    name: Joi.string()
+      .min(3)
+      .max(50)
+      .required()
+  };
+  const genre = genres.find(g => g.id === req.params.id);
+  if (!genre) return res.status(404).send("invalid id");
+  const { error } = Joi.validate(req.body, schema);
+  if (error) return res.send(error.details[0].message);
+  genre.name = req.body.name;
+  res.send(genre.name);
+});
+//deleting created genre
+app.delete("/genres/:id", (req, res) => {
+  const genre = genres.find(g => g.id === req.params.id);
+  if (!genre) return res.send("invalid id");
+  genres.shift(genre);
+  res.send(genre);
+});
 
-//app.put();
-// app.delete();
 let port = process.env.PORT || 3000;
 app.listen(port, () => {
   return console.log(`listening to port ${port}...`);
