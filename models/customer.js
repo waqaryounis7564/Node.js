acc4be11adaf6d1f62bdbc9926a794ad9fe1268e
waqar younis <waqaryounis7564@gmail.com>
2019-09-09 12:27:28 +0500

    adding models folder

---
+++ models/customer.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+const Joi = require("joi");
+
+function validateCustomer(body) {
+  const schema = {
+    name: Joi.string()
+      .min(3)
+      .max(50)
+      .required(),
+    mobile: Joi.string()
+      .required()
+      .max(13)
+  };
+  return Joi.validate(body, schema);
+}
+const customerSchema = new mongoose.Schema({
+  name: { type: String, required: true, minlength: 3, maxlength: 50 },
+  mobile: { type: String, required: true, maxlength: 15 },
+  isGold: { type: Boolean, default: false }
+});
+const Customer = mongoose.model("customer", customerSchema);
+
+exports.Customer = Genre;
+exports.validate = validateCustomer;
---
+++ models/genre.js
@@ -0,0 +1,21 @@
+const Joi = require("joi");
+const mongoose = require("mongoose");
+
+function validateGenre(body) {
+  const schema = {
+    name: Joi.string()
+      .min(3)
+      .max(50)
+      .required()
+  };
+  return Joi.validate(body, schema);
+}
+
+const genreSchema = new mongoose.Schema({
+  name: { type: String, required: true, minlength: 3, maxlength: 50 }
+});
+
+const Genre = mongoose.model("genre", genreSchema);
+
+exports.Genre = Genre;
+exports.validate = validateGenre;
